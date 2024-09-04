f7a958e70d1fb2b1687bac0e64fed7b0f8af97d6
hiraryw64 <hiraryw64@gmail.com>
2024-09-04 09:44:17 +0200

    Arreglando errores varios con respecto al commit anterior

--- public/javaScript/addParticipantes/index.js
+++ public/javaScript/addParticipantes/index.js
@@ -4,15 +4,38 @@ contFormulario.style.visibility = "hidden";
 
 
 botonPrueba.addEventListener("click", () => {
+    let contFormulario = document.getElementById("contFormulario");
+    let fechaAcogida = document.getElementById("f_Acogida").value;
+    let nombreParticipante = document.getElementById("nombre").value;
+    let apellido1 = document.getElementById("apellido1").value;
+    let apellido2 = document.getElementById("apellido2").value;
+    let dni = document.getElementById("dni").value;
+    let genero = document.getElementById("genero").value;
+    let fechaNac = document.getElementById("fechaNac").value;
+    let fechaEspana = document.getElementById("fechaEspana").value;
+    let estadoCivil = document.getElementById("estadoCivil").value;
+    let paisNacimiento = document.getElementById("paisNacimiento").value;
+    let paisNacionalidad = document.getElementById("paisNacionalidad").value;
+    let direccion = document.getElementById("direccion").value;
+    let ciudad = document.getElementById("ciudad").value;
+    let telefono = document.getElementById("telefono").value;
+    let email = document.getElementById("email").value;
+
+    let botonPrueba = document.getElementById("prueba");
+    let resultado = document.getElementById("resultado");
+
+
     let proteccionDatos = document.querySelector('input[name="proteccionDatos"]:checked');
 
-    if (proteccionDatos.value == 0){
+    if (proteccionDatos.value == 0) {
         contFormulario.style.visibility = "visible";
         let empadronamiento = document.querySelector('input[name="empadronamiento"]:checked');
-    resultado.textContent += estadoCivil.value;
-        
+        let nombreNuevo = nombreParticipante;
+        resultado.textContent += apellido1;
+        resultado.textContent += nombreNuevo;
+
     } else {
         contFormulario.style.visibility = "hidden";
     }
-    
+
 });
